6f123afadf17ff477a3e947a9447b551bd13b761
Thenu Kaluarachchi <thenujaya123@gmail.com>
2023-03-17 23:49:10 +0000

    database model

---
+++ readme.md
@@ -0,0 +1,5 @@
+Creating a new model:
+    php artisan make:model NAME --migration
+    to add fields to the model, we edit the migration file
+    php artisan migrate
+    enabled sqlite3 extension in php.ini
--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/test', function () {
-    return 'Hello World';
+Route::get('/products', function () {
+    return 'products';
 });
